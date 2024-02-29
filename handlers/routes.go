5192c7a34b6f0a541c3d9f8d1a573960f8808f78
Marc Michel <marc@michel.lu>
2024-02-29 20:29:11 +0100

    feat: add simple healtcheck endpoint

--- handlers/routes.go
+++ handlers/routes.go
@@ -1,8 +1,23 @@
 package handlers
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func SetupRoutes(app *echo.Echo, h *UserHandler) {
+	healthcheckRoute(app)
+	userRoutes(app, h)
+}
+
+func healthcheckRoute(app *echo.Echo) {
+	app.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Service is healthy!")
+	})
+}
+
+func userRoutes(app *echo.Echo, h *UserHandler) {
 	group := app.Group("/users")
 
 	group.POST("/add", h.HandlerAddUser)
