f570a565763db9d8c51bde0aef1d41cd4d076d7c
Marc Michel <marc@michel.lu>
2024-05-05 20:42:48 +0200

    test: add e2e logout test

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
       "cypress/e2e/setup/start.cy.ts",
       "cypress/e2e/pages/*.cy.ts",
       "cypress/e2e/api/*.cy.ts",
-      "cypress/e2e/base-layout/*.cy.ts",
+      "cypress/e2e/layout/*.cy.ts",
       "cypress/e2e/setup/stop.cy.ts",
     ],
   },
